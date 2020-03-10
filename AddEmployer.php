8b1469716bc44fdf0040ae14b2d3fc22512ab6ee
6abrielMR <gabriel.montes716@gmail.com>
2020-03-10 10:50:12 -0500

    AddEmployer diseño finalizado

---
+++ AddEmployer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employer | Employers</title>
+
+    <link rel="stylesheet" href="css/employer_stl.css" type="text/css">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+
+<body id="as">
+    <header>
+        <div id="top-nav"></div>
+        <div class="main-nav-desktop"></div>
+    </header>
+    <main>
+        <div id="main-form">
+            <h2>Registrar Empleado</h2>
+
+            <input class="fields" type="text" name="names" id="names" placeholder="Ingresa tus nombres">
+            <input class="fields" type="text" name="lastnames" id="lastnames" placeholder="Ingresa tus apellidos">
+            <input class="fields" type="text" name="number-id" id="number-id" placeholder="Ingresa tu número de identificación">
+            <input class="fields" type="text" name="number-one" id="number-one" placeholder="Ingresa un número de télefono">
+            <input class="fields" type="text" name="number-two" id="number-two" placeholder="Ingresa un número de télefono">
+            <input class="fields" type="text" name="number-three" id="number-three" placeholder="Ingresa un número de télefono">
+            <input class="fields" type="text" name="email" id="email" placeholder="Ingresa un correo eléctronico">
+            <input class="fields" type="text" name="salary" id="salary" placeholder="Ingresa tu salario">
+            <div id="main-btns">
+                <input class="btn" type="submit" value="Registrar Empleado">
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -19,7 +19,6 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
